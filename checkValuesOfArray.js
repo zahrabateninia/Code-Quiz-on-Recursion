2553c2dfd8fd33e40c3c3ac899e668db2d17bc4a
zahrabateninia <zahrabatenin@gmail.com>
2024-02-23 10:49:00 +0330

    Change var declaration to let
    when you declare a variable with let, it's also hoisted to the top of its block scope (block-scoped variables), but unlike var,
    it's not initialized until the actual declaration statement is encountered in the code. This is known as the "temporal dead zone", during which accessing the variable will result in a ReferenceError.
    
    By declarig copy with let and initializing it at the same time we fix the issue

--- checkValuesOfArray.js
+++ checkValuesOfArray.js
@@ -2,13 +2,13 @@
 
 // Write a function called all which accepts an array and a callback and returns true if every value in the array returns true when passed as parameter to the callback function
 
-var allAreLessThanSeven = all([1,2,9], function(num){
+let allAreLessThanSeven = all([1,2,9], function(num){
     return num < 7
 });
 
 function all(arr, callback){
     // Initialize copy with a shallow copy of the array only if copy is falsy ( undefined, NULL, false, 0, NAN)
-    var copy = copy || arr.slice() // Create a shallow copy of the original array
+    let copy =  arr.slice() // Create a shallow copy of the original array
     // base case of recursive function:
     if (copy.length === 0) return true;
 
