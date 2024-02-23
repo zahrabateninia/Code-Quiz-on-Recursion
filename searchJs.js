dd279c7adcd57b395f87c4ffced6b4997facda8a
zahrabateninia <zahrabatenin@gmail.com>
2024-02-23 11:37:04 +0330

    Add the sample of search JS question

---
+++ searchJs.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+// Write a function called contains that searches for a value in a nested object. It returns true if the object contains that value.
+
+var nestedObject = {
+    data: {
+        info: {
+            stuff: {
+                thing: {
+                    moreStuff: {
+                        magicNumber: 44,
+                        something: 'foo2'
+                    }
+                }
+            }
+        }
+    }
+}
+
+let hasIt = contains(nestedObject, 44); // true
+let doesntHaveIt = contains(nestedObject, "foo"); // false
