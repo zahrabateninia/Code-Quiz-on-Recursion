2c19ea0830dd9068ef095a79469cc00982ff71b6
zahrabateninia <zahrabatenin@gmail.com>
2024-02-23 09:54:17 +0330

    Write a sample for checking all values of an array question

---
+++ checkValuesOfArray.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node 
+
+// Write a function called all which accepts an array and a callback and returns true if every value in the array returns true when passed as parameter to the callback function
+
+var allAreLessThanSeven = all([1,2,9], function(num){
+    return num < 7
+});
+
+console.log(allAreLessThanSeven())
+
+function all(arr, callback){
+    
+    
+}
