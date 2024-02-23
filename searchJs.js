be0883288dee5d1c07ff496eb0036e5b1da641e7
zahrabateninia <zahrabatenin@gmail.com>
2024-02-23 12:33:13 +0330

    Add more comments

--- searchJs.js
+++ searchJs.js
@@ -21,15 +21,16 @@ let hasIt = contains(nestedObject, 44); // true
 let doesntHaveIt = contains(nestedObject, "foo"); // false
 
 function contains(object, searchValue){
+    // base case: if the one of the conditions is met, it means that the object is not an object itself, but rather a primitive value or null
     if(typeof object !== "object" || object === null ){
         return searchValue === object; 
     }
 
-    // Recursive case: if the object is an object (but not null), iterate over its values
-    for (const value of Object.values(object)) {
+    // Recursive case: if the object is an Object (but not null), iterate over its values
+    for (const value of Object.values(object)) { // Object.values(object) takes an object and returns a list containing object's values
         // Recursively call contains for each value
         if (contains(value, searchValue)) {
-        return true; // If the searchValue is found in any nested object, return true
+            return true; // If the searchValue is found in any nested object, return true
         }
     }
 
