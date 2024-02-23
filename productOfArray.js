1b39e288a6c877976c0e5ef490b659504053b68a
zahrabateninia <zahrabatenin@gmail.com>
2024-02-23 11:22:38 +0330

    Take an array of numbers and return the product of them useing recursive function

---
+++ productOfArray.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node 
+
+// Write a function called productOfArray which takes in an array of numbers and returns the product of them all
+
+let six = productOfArray([1,2,3]) // 6
+let sixty = productOfArray([1,2,3,10]) // 60
+
+function productOfArray(arr){
+    // base case
+    if(arr.length === 0) return 1;
+
+    return arr.shift() * productOfArray(arr)
+}
+
+console.log(six, sixty)
